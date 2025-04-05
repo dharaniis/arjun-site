8147d11f898f3e597d9234035219fef1e05ee1f6
Dharani I S <ponnidharani@gmail.com>
2025-04-05 23:47:33 +0530

    add landing responsive

--- client/src/pages/home/Landing.tsx
+++ client/src/pages/home/Landing.tsx
@@ -4,9 +4,9 @@ const Landing = () => {
   return (
     <div id="Landing" className="relative flex justify-center items-center h-screen overflow-hidden">
       <div className="flex flex-col gap-5 justify-center items-center text-center">
-        <h1 className="text-7xl">"Your Fitness Odyssey Begins Here"</h1>
-        <h1 className="text-xl">ARJUN KUMAR - ONLINE FITNESS COACH</h1>
-        <AnchorLink href="#Programs" className="w-fit text-sm px-7 py-2.5 text-red-600 hover:text-white border border-red-600 hover:bg-red-600 focus:ring-2 focus:ring-red-300" >Get Started</AnchorLink>
+        <h1 className="text-[50px] leading-none md:text-7xl">"Your Fitness Odyssey Begins Here"</h1>
+        <h1 className="text-sm md:text-xl">ARJUN KUMAR - ONLINE FITNESS COACH</h1>
+        <AnchorLink href="#Programs" className="w-fit md:text-sm text-xs px-5 py-2 md:px-7 md:py-2.5 text-red-600 hover:text-white border border-red-600 hover:bg-red-600 focus:ring-2 focus:ring-red-300" >Get Started</AnchorLink>
       </div>
       <video className="absolute object-cover h-full w-full top-0 left-0 opacity-30 z-[-1]" autoPlay muted loop id="myVideo">
         <source src="/src/assets/gym.mp4" type="video/mp4" />
