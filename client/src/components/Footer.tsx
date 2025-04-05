e2fd78b635d61e5c55c4232f283bc04948922a43
Dharani I S <ponnidharani@gmail.com>
2025-04-05 23:41:33 +0530

    add footer responsive

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,29 +1,30 @@
 import { Link } from "react-router-dom"
+import AnchorLink from "react-anchor-link-smooth-scroll"
 
 type Props = {}
 
 function Footer({}: Props) {
   return (
     <div id="Footer" className="relative bg-red-600 text-black text-center font-bold font-sans">
-        <div className="grid grid-cols-5 justify-center items-center p-16">
-            <div className="flex justify-start">
+        <div className="grid grid-cols-1 gap-5 md:grid-cols-5 justify-center items-center p-16">
+            <div className="flex justify-center  md:justify-start">
                 <ul>
                     <li><Link to="/">Home</Link></li>
-                    <li><Link to="/">About</Link></li>
-                    <li><Link to="/">Programs</Link></li>
+                    <li><Link to="/about">About</Link></li>
+                    <li><AnchorLink href="#Programs">Programs</AnchorLink></li>
                     <li><Link to="/">Blog</Link></li>
-                    <li><Link to="/">Contact</Link></li>
+                    <li><Link to="/contact">Contact</Link></li>
                 </ul>
             </div>
-            <div className="col-span-3">
-                <a href="/" className="text-10xl goldman">Arjun</a>
+            <div className="md:col-span-3 order-first md:order-none">
+                <a href="/" className="text-8xl md:text-10xl goldman">Arjun</a>
             </div>
-            <div className="flex justify-end">
+            <div className="flex justify-center md:justify-end">
                 <ul>
                     <li className="font-bold text-xl"><span className="border-b-2 border-black">Programs</span></li>
-                    <li><Link to="/">Fatloss</Link></li>
-                    <li><Link to="/">Bulking</Link></li>
-                    <li><Link to="/">Body Recomposition</Link></li>
+                    <li><Link to="/program/fatloss">Fatloss</Link></li>
+                    <li><Link to="/program/bulking">Bulking</Link></li>
+                    <li><Link to="/program/bodyRecomposition">Body Recomposition</Link></li>
                 </ul>
             </div>
             <h1 className="w-fit absolute bottom-2 left-0 right-0 mx-auto text-xs">© 2035 by Arjun Kumar. Powered and secured by Dharani I S</h1>
