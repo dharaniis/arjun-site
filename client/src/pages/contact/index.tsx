10654c2e7072553f41f954a171ef2cf57badfc77
Dharani I S <ponnidharani@gmail.com>
2025-04-06 18:15:03 +0530

    add contact page responsive

--- client/src/pages/contact/index.tsx
+++ client/src/pages/contact/index.tsx
@@ -9,12 +9,12 @@ function Contact({}: Props) {
     return (
         <div>
         <Navbar /> 
-        <div className="py-32 space-y-10">
+        <div className="mx-5 py-32 space-y-10">
             <div className="mx-auto space-y-2 text-center">
-                <h1 className="text-7xl text-red-600 ">Have a question?<br/>Shoot us an email</h1>
-                <p className="text-base font-sans">We are an industry-leading publication that provides the latest news and insights about SEO, search, and how it impacts your business-and career.<br/>Have a question for us or feedback? Please click on the most appropriate category and fill out the form to reach us.</p>
+                <h1 className="text-6xl md:text-7xl text-red-600 ">Have a question?<br/>Shoot us an email</h1>
+                <p className="text-[10px] md:text-base font-sans">We are an industry-leading publication that provides the latest news and insights about SEO, search, and how it impacts your business-and career.<br/>Have a question for us or feedback? Please click on the most appropriate category and fill out the form to reach us.</p>
             </div>
-            <div className="mx-auto w-1/3">
+            <div className="mx-auto w-84">
                 <ContactForm />
             </div>
         </div>
