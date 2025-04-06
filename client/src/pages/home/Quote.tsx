1a9ecf3526502d2fc9e45b51a1aa10fc1cd742ab
Dharani I S <ponnidharani@gmail.com>
2025-04-06 09:22:15 +0530

    add quote responsive

--- client/src/pages/home/Quote.tsx
+++ client/src/pages/home/Quote.tsx
@@ -3,8 +3,8 @@ type Props = {}
 
 function Quote({}: Props) {
   return (
-    <div id="Quote" className="bg-red-700 h-[180px] flex justify-center items-center drop-shadow-xl">
-        <h1 className="text-4xl font-thin font-sans text-center">"WHAT SEEMS IMPOSSIBLE TODAY WILL SOON BECOME YOUR WARM-UP"</h1>
+    <div id="Quote" className="bg-red-700 h-[150px] md:h-[180px] flex justify-center items-center drop-shadow-xl">
+        <h1 className="text-[1.3rem] md:text-4xl font-thin font-sans text-center">"WHAT SEEMS IMPOSSIBLE TODAY WILL SOON BECOME YOUR WARM-UP"</h1>
     </div>
   )
 }
