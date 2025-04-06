fb5887f65149a4fdc00b7d2e4b754d28fd10ebe8
Dharani I S <ponnidharani@gmail.com>
2025-04-06 18:02:19 +0530

    add about programs pages responsive

--- client/src/pages/program/index.tsx
+++ client/src/pages/program/index.tsx
@@ -28,15 +28,15 @@ function Program({}: Props) {
     return (
       <div>
           <Navbar /> 
-          <div className="pt-32 flex flex-col gap-5 items-center text-center">
-            <div>
-                <h1 className="leading-[2.5rem] text-[55px]">{programs[programName].tagline}<br/>in just</h1>
-                <h1 className="text-red-600 text-[145px]">120 days</h1>
-                <p className="text-lg font-sans">The Step-by-Step 120 days transformation program that will guide you to<br/>lose fat through home workouts or gym workouts combined with 300+ diet plans</p>
+          <div className="mx-4 pt-32 space-y-5 flex flex-col gap-5 items-center text-center">
+            <div className="space-y-5">
+                <h1 className="leading-[2.5rem] text-4xl md:text-[55px]">{programs[programName].tagline}<br/>in just</h1>
+                <h1 className="text-red-600 text-8xl md:text-[145px]">120 days</h1>
+                <p className="text-sm md:text-lg font-sans">The Step-by-Step 120 days transformation program that will guide you to<br/>lose fat through home workouts or gym workouts combined with 300+ diet plans</p>
             </div>
-            <button type="button" className="w-[50rem] text-xl border-2 px-5 py-2 mt-0 m-5 border-red-600 goldman hover:bg-red-600 hover:text-white">CLICK HERE TO JOIN</button>
+            <button type="button" className="md:w-[50rem] w-fit text-sm md:text-xl border-2 px-3 py-2 md:px-5 md:py-2 mt-0 m-5 border-red-600 goldman hover:bg-red-600 hover:text-white">CLICK HERE TO JOIN</button>
             <div id="iframe" className="flex items-center justify-center">
-                <iframe className="m-5 w-[672px] h-[378px]" width="672" height="378" src={ytLink} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                <iframe className="m-5 w-[448px] h-[252px] md:w-[672px] md:h-[378px]" width="672" height="378" src={ytLink} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
             </div>
           </div>
           <Testimonials />
