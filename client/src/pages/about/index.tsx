2cdb6239092995cf1e4376f88cfda5a5c99029dc
Dharani I S <ponnidharani@gmail.com>
2025-04-06 17:39:36 +0530

    add about page responsive

--- client/src/pages/about/index.tsx
+++ client/src/pages/about/index.tsx
@@ -7,12 +7,12 @@ function About({}: Props) {
     return (
         <div>
             <Navbar />
-            <div className="px-24 py-36">
+            <div className="text-center px-12 py-36 space-y-10">
                 <div>
                     <h1 className="text-7xl">ARJUN KUMAR</h1>
                     <p className="text-xl text-red-600">Fitness Coach, Fitness Youtuber, Self-made Athlete</p>
                 </div>
-                <div className="flex">
+                <div className="grid grid-cols-1 md:grid-cols-2">
                     <div className="mt-16 font-sans text-left basis-1/2">
                         <p>Hey! I am Tharun, a fitness science nerd, self-made athlete and a health coach. I trained 300+ people from across India to tone up, get lean abs, lose fat and build a body they feel great about.</p> ‍
                         <p> My fitness journey has been a rollercoaster ride. I was lean and weighed 47 kgs when I first began the workout to bulk up. I did gain muscles, but unnecessary fat too. Why? Because of the lack of the right kind of training and abundance of scattered, untrue information available on the internet.</p> ‍
@@ -22,7 +22,7 @@ function About({}: Props) {
                         <p>I took the road to a healthy life in 2016 and since then, I never looked back to my tired and unfit self. I expertise in fat loss, muscle gain and being in shape. My current obsession is to help people build a strong body and craft a healthier life. Step by step.</p> ‍
                         <p>After launching several successful transformation stories on the planet, I started a Youtube channel with the sole purpose to help more people become fit. We are now trusted by 110k+ people as subscribers. I am excited to work with you in FitX-120.</p>
                     </div>
-                    <div className="basis-1/2 flex justify-center items-center">
+                    <div className="order-first md:order-none flex justify-center items-center">
                         <img className="w-[30rem] object-cover" src="/src/assets/saketabout.jpg"></img>
                     </div>
                 </div>
