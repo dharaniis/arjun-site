098cb3769123beca2e8f48898cbaa0d4a955e70f
Dharani I S <ponnidharani@gmail.com>
2025-04-06 09:41:32 +0530

    add aboutMe responsive

--- client/src/pages/home/AboutMe.tsx
+++ client/src/pages/home/AboutMe.tsx
@@ -2,16 +2,16 @@ type Props = {}
 
 function AboutMe({}: Props) {
   return (
-    <div id="AboutMe" className="flex h-screen">
-        <div className="basis-1/2 relative">
+    <div id="AboutMe" className="grid grid-cols-1 md:grid-cols-2 h-fit">
+        <div className="relative h-screen">
             <img className="absolute top-0 left-0 object-cover h-full w-full opacity-50" src="/src/assets/saket.jpg"></img>
         </div>
-        <div className="mx-10 basis-1/2 flex flex-col justify-center items-start ">
+        <div className="m-7 md:mx-10 flex flex-col justify-center items-center text-center md:text-left md:items-start">
             <div className="">
                 <h1 className="text-3xl text-red-600">About Me</h1>
                 <h1 className="text-5xl font-bold">Arjun Kumar</h1>
             </div>
-            <p className="mt-10 w-[25rem] font-sans">I'm a paragraph. Click here to add your own text and edit me. Its easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. Feel free to drag and drop me anywhere you like on your page.<br/><br/>Im a great place for you to tell a story and let your users know a little more about you.</p>
+            <p className="mt-10 w-full md:w-[25rem] font-sans">I'm a paragraph. Click here to add your own text and edit me. Its easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. Feel free to drag and drop me anywhere you like on your page.<br/><br/>Im a great place for you to tell a story and let your users know a little more about you.</p>
         </div>
     </div>
   )
