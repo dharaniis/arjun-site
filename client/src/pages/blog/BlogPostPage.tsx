e66446965e4110b129b952e3ca403f13d3f8a372
Dharani I S <ponnidharani@gmail.com>
2025-05-17 12:22:19 +0530

    add back button blogPostPage

--- client/src/pages/blog/BlogPostPage.tsx
+++ client/src/pages/blog/BlogPostPage.tsx
@@ -1,8 +1,9 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { BlogPostType } from '../../shared/types'
 import { useEffect, useState } from 'react'
 import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
+import { ArrowLeftIcon } from "@heroicons/react/24/solid";
 
 type Props = {}
 
@@ -22,7 +23,8 @@ function BlogPostPage({}: Props) {
     <div className="bg-white">
       <Navbar/>
       <div className="py-24 flex justify-center items-center text-black font-sans text-center">
-        <div className="w-[60%] space-y-10 px-20 py-12 h-fit border border-black ">
+        <div className="relative w-[60%] space-y-10 px-20 py-12 h-fit border border-black ">
+          <Link to="/blog"><ArrowLeftIcon className="w-14 h-14 top-[7rem] left-[-8rem] mr-10 absolute fill-red-600"/></Link>
           <p className="text-left w-full text-sm">{`Admin • ${postObj?.date} • ${postObj?.duration} min read`}</p>
           <h1 className="text-5xl font-bold">{postObj?.title}</h1>
           <p className="text-sm md:text-[17px]/8">{postObj?.content}</p>
